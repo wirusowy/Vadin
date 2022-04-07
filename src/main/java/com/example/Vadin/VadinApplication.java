55d276318e617be3764952c3bdc8f870c7edd6ff
wirusowy <wirusowy@users.noreply.github.com>
2022-04-07 17:53:30 +0200

    Initial commit

---
+++ src/main/java/com/example/Vadin/VadinApplication.java
@@ -0,0 +1,13 @@
+package com.example.Vadin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VadinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VadinApplication.class, args);
+	}
+
+}
